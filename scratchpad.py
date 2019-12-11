243c4b850ab62a94ec56572bfc8f99ad38162944
rtohid <tohid.rod@gmail.com>
2019-12-11 22:53:58 +0000

    EXAMPLE: generate control-flow graph.

--- scratchpad.py
+++ scratchpad.py
@@ -1,24 +1,12 @@
-import random
+from fuzzing_code.ControlFlow import gen_cfg, to_graph
+from graphviz import Source
+import inspect
 
+def my_function(a, b):
+    return a + b
 
-# Exercise 1
-# https://www.fuzzingbook.org/html/Intro_Testing.html#Exercise-1:-Testing-Shellsort
-def shellsort(elems):
-    sorted_elems = elems.copy()
-    gaps = [701, 301, 132, 57, 23, 10, 4, 1]
-    for gap in gaps:
-        for i in range(gap, len(sorted_elems)):
-            temp = sorted_elems[i]
-            j = i
-            while j >= gap and sorted_elems[j - gap] > temp:
-                sorted_elems[j] = sorted_elems[j - gap]
-                j -= gap
-            sorted_elems[j] = temp
+cfg = gen_cfg(inspect.getsource(my_function))
+print(cfg)
+graph = to_graph(cfg)
+Source(graph)
 
-    return sorted_elems
-
-
-random_length = random.sample(range(1, 11), 10)
-for length in random_length:
-    test_case = random.sample(range(length + 1), length)
-    assert sorted(test_case) == shellsort(test_case)
