13db79e5b6ff3153026b12660302038f48e973fc
R. Tohid <tohid.rod@gmail.com>
2020-01-30 00:19:58 -0600

    Add the target example.
    The simplest example for the shortest optimization pipeline.

---
+++ target.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2020 R. Tohid
+#
+# Distributed under the Boost Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+from __future__ import absolute_import
+
+from phyflow.flow.control import phyfn
+
+import numpy
+
+N = 4
+a = numpy.zeros(N, dtype=int)
+b = numpy.zeros(N, dtype=int)
+c = numpy.zeros(N, dtype=int)
+
+for i in range(N):
+    a[i] = i
+    b[i] = 2 * i + 1
+
+
+@phyfn
+def vector_vector_add_int(a, b, c):
+
+    for i in range(N):
+        c[i] = a[i] + b[i] - i
+
+    return c
+
+
+print(vector_vector_add_int(a, b, c))
