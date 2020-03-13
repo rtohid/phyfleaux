0cdfbeae4eb5ff235e72d33eb3adf2d2e812c74e
R. Tohid <tohid.rod@gmail.com>
2020-03-13 21:37:14 +0000

    Demo: start.

---
+++ phylanx/analysis/nlt.py
@@ -0,0 +1,13 @@
+"""Single Entry Single Exit"""
+
+from __future__ import absolute_import
+
+_license__ = """ 
+Copyright (c) 2020 R. Tohid
+
+Distributed under the Boost Software License, Version 1.0. (See accompanying
+file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+"""
+
+def build_scop_depth(parameter_list):
+    pass
--- target.py
+++ target.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 from flow import flowfn
 
 import numpy
+<<<<<<< HEAD
 
 N = 4
 a = numpy.zeros(N, dtype=int)
@@ -26,6 +27,25 @@ def vector_vector_add_int(a, b, c):
         c[i] = a[i] + b[i] - i
 
     return c
+=======
+from phylanx.core.directives import Phylanx
+
+@Phylanx
+def vector_vector_int_add():
+    a = numpy.zeros(10, dtype=int)
+    b = numpy.zeros(10, dtype=int)
+    c = numpy.zeros(10, dtype=int)
+
+    for i in range(10):
+        a[i] = i
+        b[i] = 2 * i + 1
+
+    for i in range(10):
+        c[i] = a[i] + b[i] - i
+
+import astpretty
+astpretty.pprint(vector_vector_int_add.fn.python_ast)
+>>>>>>> 6dc5820... Demo: start.
 
 
 # print(vector_vector_add_int(a, b, c))
