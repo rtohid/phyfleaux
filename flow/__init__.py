9f65a89efbfc43e15c616130a5d8c763cc94d698
rtohid <tohid.rod@gmail.com>
2020-04-23 18:55:14 +0000

    Remove unnecessary files.

--- flow/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2020 R. Tohid
-#
-# Distributed under the Boost Software License, Version 1.0. (See a copy at
-# http://www.boost.org/LICENSE_1_0.txt)
-
-from __future__ import absolute_import
-
-from flow.ir.utils import flowfn
--- flow/ir/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) 2020 R. Tohid
-#
-# Distributed under the Boost Software License, Version 1.0. (See a copy at
-# http://www.boost.org/LICENSE_1_0.txt)
