ee8b85142d77aacffb8c52b65da9c60fd96c1f64
ct.clmsn <ct_clmsn_at_gmail_dot_com>
2020-04-23 00:41:53 -0400

    tutorial b support

--- backend/tutorial_b.py
+++ backend/tutorial_b.py
@@ -3,41 +3,35 @@
 #  Distributed under the Boost Software License, Version 1.0. (See accompanying
 #  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #
-from pytiramisu import init_physl, var, expr, constant, function, primitive_t, argument_t, buffer, computation, codegen_physl, p_uint8, a_output, a_input, input
+from pytiramisu import init_physl, var, expr, constant, function, primitive_t, argument_t, buffer, computation, codegen_physl, p_uint8, a_output, a_input, input, cast, p_int32, int32_expr, uint8_expr
 
 if __name__ == "__main__":
 
-    '''
-    '''
+    NN = int32_expr(10)
+    MM = int32_expr(20)
 
-    NN = 10
-    MM = 20
+    i = var("i", int32_expr(0), NN)
+    j = var("j", int32_expr(0), MM)
 
     init_physl("tut_02")
 
-    N = constant("N", expr(NN))
-    M = constant("M", expr(MM)) # expr needs to wrap all integers going into these constructors
+    A = input("A", list(["i", "j"]), list([NN, MM]), p_uint8)
 
-    A = input("A", list(["i", "j"]), list([expr(NN), expr(MM)]), p_uint8)
-
-    i = var("i", expr(0), N)
-    j = var("j", expr(0), M)
-
-    output = computation("output", list([i, j]), A(i, j) + cast(p_uint8, i) + uint8(4))
+    output = computation("output", list([i, j]), A(i, j) + cast(p_uint8, i) + uint8_expr(4))
 
     i0 = var("i0")
     i1 = var("i1")
     j0 = var("j0")
     j1 = var("j1")
 
-    output.tile(i, j, expr(2), expr(2), i0, j0, i1, j1)
+    output.tile(i, j, 2, 2, i0, j0, i1, j1)
     output.parallelize(i0)
 
-    b_A = buffer("b_A", list([expr(NN), expr(MM)]), p_uint8, a_input)
-    b_output = buffer("b_output", list([expr(NN), expr(MM)]), p_uint8, a_output)
+    b_A = buffer("b_A", list([NN, MM]), p_uint8, a_input)
+    b_output = buffer("b_output", list([NN, MM]), p_uint8, a_output)
 
     A.store_in(b_A)
     output.store_in(b_output)
 
-    physl_str = codegen_physl(buffers)
+    physl_str = codegen_physl(list([b_A, b_output]))
     print(physl_str)
