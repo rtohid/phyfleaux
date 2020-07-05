7488371d699fa79aef9ceeb251522dde248d45ec
rtohid <tohid.rod@gmail.com>
2020-07-05 20:29:34 +0000

    Start restructuring the source tree.

--- phyfleaux/api/__init__.py
+++ phyfleaux/api/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 __license__ = """
 Copyright (c) 2020 R. Tohid
 
@@ -10,3 +12,5 @@ UI to:
 - parallel and distributed implementations of commonly used packages like NumPy.
 - direct the scheduling and code genration of tasks.
 """
+
+import phyfleaux.api._numpy as numpy
