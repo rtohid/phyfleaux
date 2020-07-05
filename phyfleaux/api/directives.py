753fcf0c4bd4587c794b742c90b97b4a1c55944d
rtohid <tohid.rod@gmail.com>
2020-07-05 20:35:29 +0000

    Update directives' intefce.

--- phyfleaux/api/directives.py
+++ phyfleaux/api/directives.py
@@ -10,21 +10,29 @@ file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 import ast
 
 from typing import Callable
-
-from phyfleaux.task import Task
-from phyfleaux.polytope import Polytope
-
-
-def task(fn: Callable) -> Callable:
-    return Task(fn)
+from types import FunctionType
+from phyfleaux.task.task import Task
+# from phyfleaux.optimization.task import Polytope
 
 
-def polyhedral(fn: Callable) -> Callable:
+def task(__task_arg=None, **kwargs):
+
+    if callable(__task_arg):
+        return Task(__task_arg)
+    elif __task_arg is not None:
+        raise TypeError(
+            f"Function {task} expects {FunctionType} or {Task}"
+        )
+    else:
+        return Task
+
+
+def polyhedral(fn: [Task, FunctionType]) -> Task:
     """Attempts to detect SCoPs and apply polyhedral transformations.
-    
+
     :arg fn: python function.
 
-    Directs Phyfleaux to apply polyhedral transformations on affine iteration 
+    Directs Phyfleaux to apply polyhedral transformations on affine iteration
     spaces in :func:`fn`.
 
     reads:
@@ -34,5 +42,7 @@ def polyhedral(fn: Callable) -> Callable:
     https://en.wikipedia.org/wiki/Polytope_model
     https://en.wikipedia.org/wiki/Affine_space
     """
+
+    task_ = task(fn)
 
     return Polytope(fn)
---
+++ phyfleaux/task/task.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import
+from __future__ import annotations
+
+__license__ = """
+Copyright (c) 2020 R. Tohid
+
+Distributed under the Boost Software License, Version 1.0. (See accompanying
+file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+"""
+
+import ast
+import dis
+import inspect
+
+from copy import deepcopy
+from types import FunctionType
+
+
+class Task:
+    def __init__(self, fn: [FunctionType, Task]) -> None:
+        if isinstance(fn, FunctionType):
+
+            # Funtion
+            self.fn = deepcopy(fn)
+            self.id = self.fn.__hash__()
+
+            self.py_code = fn.__code__
+            self.py_ast = ast.parse((inspect.getsource(fn)))
+
+            # Function Signature
+            self.signature = inspect.signature(fn)
+            self.args_spec = inspect.getfullargspec(fn)
+
+            # Function Source
+            self.src = inspect.getsource(fn)
+            self.src_file, self.start_lineno = inspect.findsource(self.fn)
+            self.src_file = ''.join(map(str, self.src_file))
+            self.src_file_dir = '/'.join(inspect.getfile(fn).split('/')[:-1])
+            self.src_file_name = inspect.getfile(fn).split('/')[-1]
+
+        else:  # isinstance(fn, Task)
+            self = deepcopy(fn)
+
+        self.called = 0
+
+    def __call__(self, *args, **kwargs):
+        # inspect.signature(fn).bind()
+        self.called += 1
+        return self.fn(*args, **kwargs)
